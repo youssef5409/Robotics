880c5039091ab960066376bfdc09c122db1d31db
1mohamedyou <1mohamedyou@GWS03918.ihdsb.CA>
2017-02-15 10:23:43 -0500

    Finished our first HelloWorld program

---
+++ src/ca/hdsb/gwss/youssef/ics3u/u1/HelloWorld.java
@@ -0,0 +1,29 @@
+/*
+ * Name: Youssef Mohamed
+ * Date: Feb 15th, 2017
+ * Version: 0.0
+ * Description: The program will display 'HelloWorld'
+ *                  to the console
+ */
+package ca.hdsb.gwss.youssef.ics3u.u1;
+
+/**
+ * Hello World Program
+ * 
+ * @author 1mohamedyou
+ */
+public class HelloWorld {
+
+    /**
+     * Method to start your code
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // in-line comment
+        
+        System.out.println("Hello World");
+        System.err.println("Task Completed");
+    }
+    
+}
