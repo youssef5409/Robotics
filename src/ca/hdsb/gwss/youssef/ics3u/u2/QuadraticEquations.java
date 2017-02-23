c3914e340409fc6d3d7e48f25d6b7fe671fe6544
1mohamedyou <1mohamedyou@GWS03918.ihdsb.CA>
2017-02-23 11:10:56 -0500

    ererg

---
+++ src/ca/hdsb/gwss/youssef/ics3u/u2/QuadraticEquations.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.hdsb.gwss.youssef.ics3u.u2;
+import static java.lang.Math.sqrt;
+import java.util.Scanner;
+double root1;
+
+public class QuadraticEquations {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner ask = new Scanner(System.in);
+        System.out.println("Enter the A value");
+        double a = ask.nextDouble();
+        System.out.println("Enter the B value");
+        double b = ask.nextDouble();
+        System.out.println("Enter the C value");
+        double c = ask.nextDouble();
+        
+        double root1 = ((b*-1) + );
+        
+    }
+    
+}
