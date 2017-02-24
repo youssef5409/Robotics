634f480a59e9d4f2b67ea1cd549974ffb36cbc5b
1mohamedyou <1mohamedyou@GWS03918.ihdsb.CA>
2017-02-24 10:19:02 -0500

    Fixed

--- src/ca/hdsb/gwss/youssef/ics3u/u2/QuadraticEquations.java
+++ src/ca/hdsb/gwss/youssef/ics3u/u2/QuadraticEquations.java
@@ -6,7 +6,6 @@
 package ca.hdsb.gwss.youssef.ics3u.u2;
 import static java.lang.Math.sqrt;
 import java.util.Scanner;
-double root1;
 
 public class QuadraticEquations {
 
@@ -16,14 +15,18 @@ public class QuadraticEquations {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner ask = new Scanner(System.in);
-        System.out.println("Enter the A value");
+        System.out.print("Enter the A value ");
         double a = ask.nextDouble();
-        System.out.println("Enter the B value");
+        System.out.print("Enter the B value ");
         double b = ask.nextDouble();
-        System.out.println("Enter the C value");
+        System.out.print("Enter the C value ");
         double c = ask.nextDouble();
         
-        double root1 = ((b*-1) + );
+        double root1 = ((b*-1) + sqrt((b*b) - 4 * (a) * (c)));
+        double root2 = ((b*-1) - sqrt((b*b) - 4 * (a) * (c)));
+        
+        System.out.println("Root 1 is " + root1);
+        System.out.println("Root 2 is " + root2);
         
     }
     
