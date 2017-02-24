06e3569f7530aaf8b8a86463bdff910cd0e195ac
1mohamedyou <1mohamedyou@GWS03918.ihdsb.CA>
2017-02-24 11:49:35 -0500

    Fixed

--- src/ca/hdsb/gwss/youssef/ics3u/u2/QuadraticEquations.java
+++ src/ca/hdsb/gwss/youssef/ics3u/u2/QuadraticEquations.java
@@ -8,22 +8,50 @@ import static java.lang.Math.sqrt;
 import java.util.Scanner;
 
 public class QuadraticEquations {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        double a = 0;
+        double b = 0;
+        double c = 0;
         // TODO code application logic here
-        Scanner ask = new Scanner(System.in);
-        System.out.print("Enter the A value ");
-        double a = ask.nextDouble();
-        System.out.print("Enter the B value ");
-        double b = ask.nextDouble();
-        System.out.print("Enter the C value ");
-        double c = ask.nextDouble();
         
-        double root1 = ((b*-1) + sqrt((b*b) - 4 * (a) * (c)));
-        double root2 = ((b*-1) - sqrt((b*b) - 4 * (a) * (c)));
+        System.out.println("Enter the A value ");
+        Scanner askA = new Scanner(System.in);
+        
+        while (!askA.hasNextDouble()) {
+            
+            System.out.println("Sorry, this A value is invalid");
+            askA.next();
+        }
+        
+        a = askA.nextDouble();
+        
+        System.out.println("Enter the B value ");
+        Scanner askB = new Scanner(System.in);
+        
+        while (!askB.hasNextDouble()) {
+            
+            System.out.println("Sorry, this B value is invalid");
+            askB.next();
+        }
+        
+        b = askB.nextDouble();
+
+        System.out.println("Enter the C value ");
+        Scanner askC = new Scanner(System.in);
+
+        while (!askC.hasNextDouble()) {
+            System.out.println("Sorry, this C value is invalid");
+            askC.next();
+        }
+        
+        c = askC.nextDouble();
+        
+        
+        double root1 = ((b * -1) + sqrt((b * b) - 4 * (a) * (c)));
+        double root2 = ((b * -1) - sqrt((b * b) - 4 * (a) * (c)));
         
         System.out.println("Root 1 is " + root1);
         System.out.println("Root 2 is " + root2);
