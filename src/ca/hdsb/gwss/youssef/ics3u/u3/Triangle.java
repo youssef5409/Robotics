e0fdf9d07ea370d4fdf7450d7e8678ba02959611
1mohamedyou <1mohamedyou@GWS03918.ihdsb.CA>
2017-03-21 09:37:41 -0400

    Finished Triangle Rain

--- src/ca/hdsb/gwss/youssef/ics3u/u3/Triangle.java
+++ src/ca/hdsb/gwss/youssef/ics3u/u3/Triangle.java
@@ -31,7 +31,7 @@ public class Triangle {
         System.out.println("What is the length of the third side: ");
         sideThree = read.nextDouble();
         
-        if ((sideOne + sideTwo) > sideThree || (sideOne + sideThree) > sideTwo ||
+        if ((sideOne + sideTwo) > sideThree && (sideOne + sideThree) > sideTwo &&
                 (sideTwo + sideThree) > sideOne) {
             System.out.println("A triangle can be made");
     }
