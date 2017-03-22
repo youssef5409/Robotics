2d56b3b75cb0ce5ab4f40a01a31af0c9f852f048
youssef5409 <1mohamedyou@hdsb.ca>
2017-03-21 21:06:27 -0400

    Added right triangle to triangle

--- src/ca/hdsb/gwss/youssef/ics3u/u3/Triangle.java
+++ src/ca/hdsb/gwss/youssef/ics3u/u3/Triangle.java
@@ -1,11 +1,14 @@
 /*
  * Name: Youssef Mohamed
- * Version: 0.1
+ * Version: 0.3
  * Date: 20th March 2017
  * Description: Determines if 3 lines can make a triangle
  */
 package ca.hdsb.gwss.youssef.ics3u.u3;
+
+import static java.lang.Math.pow;
 import java.util.Scanner;
+
 /**
  *
  * @author 1mohamedyou
@@ -20,23 +23,23 @@ public class Triangle {
         double sideTwo;
         double sideThree;
         double swap;
-        
+
         Scanner read = new Scanner(System.in);
-        
+
         System.out.print("What is the length of the first side: ");
         sideOne = read.nextDouble();
-        
+
         System.out.print("What is the length of the second side: ");
         sideTwo = read.nextDouble();
-        
+
         System.out.print("What is the length of the third side: ");
         sideThree = read.nextDouble();
-        
+
         if (sideTwo > sideOne) {
             swap = sideOne;
             sideOne = sideTwo;
             sideTwo = swap;
-    }
+        }
         if (sideThree > sideOne) {
             swap = sideOne;
             sideOne = sideThree;
@@ -44,11 +47,14 @@ public class Triangle {
         }
         if (sideOne >= (sideTwo + sideThree)) {
             System.out.println("A triangle cannot be made");
-        }
-        else {
-            
-            System.out.println("deerm");
+        } else {
+            if (pow(sideThree, 2) + pow(sideTwo, 2) == pow(sideOne, 2)) {
+                System.out.println("Not only can a Triangle be made, but it can also"
+                        + " be a Right-Angled one.");
+            } else {
+                System.out.println("A triangle can be made, however it cannot"
+                        + " be Right-Angled.");
+            }
         }
     }
-    
 }
