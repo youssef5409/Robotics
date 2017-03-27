63941f723fa3e56c5c4d5d60f3dc71a7b3a4a39a
1mohamedyou <1mohamedyou@GWS03918.ihdsb.CA>
2017-03-27 10:47:23 -0400

    Added comments

---
+++ src/discovering/graphing/GraphingInfo.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package discovering.graphing;
+import java.util.Scanner;
+/**
+ *
+ * @author 1mohamedyou
+ */
+public class GraphingInfo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double x;
+        double y;
+        String done = "";
+        System.out.println("Hi, I can make line graphs for you.");
+        Scanner read = new Scanner(System.in);
+        while (!done.equals("done")) {
+            System.out.println("Enter the x co-ordinate of your point");
+            x = read.nextDouble();
+            System.out.println("Enter the y co-ordinate of your point");
+            y = read.nextDouble();
+            System.out.println("If that was your last point, enter \"done\", if not"
+                    + "Press Enter.");
+        }
+    }
+    
+}
