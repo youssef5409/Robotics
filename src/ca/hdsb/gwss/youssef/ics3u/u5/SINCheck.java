64cf14b7f0b30164550f7f4bcef3e373613f324a
youssef5409 <1mohamedyou@hdsb.ca>
2017-04-06 20:46:17 -0400

    I can't stop coding..

--- src/ca/hdsb/gwss/youssef/ics3u/u5/SINCheck.java
+++ src/ca/hdsb/gwss/youssef/ics3u/u5/SINCheck.java
@@ -5,6 +5,9 @@
  */
 package ca.hdsb.gwss.youssef.ics3u.u5;
 
+import static java.lang.Math.ceil;
+import java.util.Scanner;
+
 /**
  *
  * @author 1mohamedyou
@@ -15,7 +18,35 @@ public class SINCheck {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String sin;
+        double total = 0;
+        int digit;
+
+        Scanner read = new Scanner(System.in);
+        do {
+        System.out.println("Enter SIN: ");
+        sin = read.nextLine();
+        }while (sin.length() < 8);
+
+        for (int i = 0; i < sin.length() - 1; i++) {
+            if (i % 2 == 0) {
+                digit = Character.getNumericValue(sin.charAt(i));
+                total += digit;
+            } else {
+                digit = 2 * Character.getNumericValue(sin.charAt(i));
+                if (digit > 10) {
+                    digit = (digit - 10) + 1;
+                }
+                total += digit;
+            }
+        }
+        if (ceil(total / 10) * 10 - total == Character.getNumericValue(sin.charAt(8))) {
+            System.out.println("Verified.");
+        } else {
+            System.out.println("Not Verified.");
+        }
     }
-    
 }
+    
+
+//130692544
