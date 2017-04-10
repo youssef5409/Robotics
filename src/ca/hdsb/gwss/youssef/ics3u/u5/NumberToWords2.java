e717dd7c3e4d47fef50bc797838d71c376775306
1mohamedyou <1mohamedyou@GWS03918.ihdsb.CA>
2017-04-10 11:28:28 -0400

    Fixed plaindrome

---
+++ src/ca/hdsb/gwss/youssef/ics3u/u5/NumberToWords2.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.hdsb.gwss.youssef.ics3u.u5;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 1mohamedyou
+ */
+public class NumberToWords2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int number;
+        Scanner read = new Scanner(System.in);
+        System.out.println("Enter Number: ");
+        number = Integer.parseInt(read.nextLine());
+        
+        
+
+    }
+    private static void ones() {
+        
+    }
+    private static void tens() {
+        
+    }
+    private static void hundreds() {
+        
+    }
+    private static void teens() {
+        
+    }
+}
