0f19e7dd12586f2e3883418f1fdcb49f145a2df6
youssef5409 <1mohamedyou@hdsb.ca>
2017-04-11 01:33:12 -0400

    I can't stop coding..

--- src/ca/hdsb/gwss/youssef/ics3u/u5/NumberToWords2.java
+++ src/ca/hdsb/gwss/youssef/ics3u/u5/NumberToWords2.java
@@ -19,7 +19,7 @@ public class NumberToWords2 {
      * @param args the command line arguments
      */
     static ArrayList words = new ArrayList();
-    static boolean onesAvailable = true;
+    static boolean teensNeeded = false;
 
     public static void main(String[] args) {
         words.add("one");
@@ -38,9 +38,10 @@ public class NumberToWords2 {
         words.add("twenty");
         words.add("thirty");
 
+        final int ORIGINAL_NUMBER;
         int number;
-        String hundreds;
-        String tens;
+        String hundreds = "";
+        String tens = "";
         String ones;
 
         Scanner read = new Scanner(System.in);
@@ -48,14 +49,25 @@ public class NumberToWords2 {
 
         System.out.print("Enter Number: ");
         number = Integer.parseInt(read.nextLine());
-
+        ORIGINAL_NUMBER = number;
         while (number > 0) {
             numbers.push(number % 10);
             number = number / 10;
         }
-        hundreds = hundreds((int) numbers.pop());
-        tens = tens((int) numbers.pop());
-        ones = ones((int) numbers.pop());
+        if (ORIGINAL_NUMBER > 99) {
+            hundreds = hundreds((int) numbers.pop());
+            tens = tens((int) numbers.pop());
+            if (!teensNeeded) {
+                ones = ones((int) numbers.pop());
+            } else {
+                ones = teens((int) numbers.pop());
+            }
+        } else if (ORIGINAL_NUMBER > 9) {
+            tens = tens((int) numbers.pop());
+            ones = ones((int) numbers.pop());
+        } else {
+            ones = ones((int) numbers.pop());
+        }
 
         System.out.println(hundreds + tens + ones);
 
@@ -67,15 +79,9 @@ public class NumberToWords2 {
 
     private static String tens(int n) {
         String word;
-        if (n < 4) {
-            onesAvailable = false;
-            word = "thirteen";
-            if (n < 3) {
-                word = "twelve";
-                if (n < 2) {
-                    word = "eleven";
-                }
-            }
+        if (n == 1) {
+            teensNeeded = true;
+            word = "";
         } else {
             if (n == 5) {
                 word = "fifty";
@@ -90,4 +96,19 @@ public class NumberToWords2 {
         return words.get(n - 1) + " hundred ";
     }
 
+    private static String teens(int n) {
+        String word;
+        if (n < 4) {
+            word = "thirteen";
+            if (n < 3) {
+                word = "twelve";
+                if (n < 2) {
+                    word = "eleven";
+                }
+            }
+        } else {
+            word = words.get(n - 1) + "ty";
+        }
+        return word;
+    }
 }
