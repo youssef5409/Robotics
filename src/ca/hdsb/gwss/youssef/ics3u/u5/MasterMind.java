c41e07ea06daa009bbf2904e745776e37fbac283
youssef5409 <1mohamedyou@hdsb.ca>
2017-04-14 21:33:42 -0400

    Gonna make mastermind using objects, then switch to methods when muir tells me to.

--- src/ca/hdsb/gwss/youssef/ics3u/u5/MasterMind.java
+++ src/ca/hdsb/gwss/youssef/ics3u/u5/MasterMind.java
@@ -14,15 +14,22 @@ import java.util.Scanner;
 public class MasterMind {
 
     public static void main(String[] args) {
+
         int guesses;
+        int guess = 0;
         int firstBlock = (int) (Math.random() * 4);
-        System.out.println(firstBlock);
         int secondBlock = (int) (Math.random() * 4);
-        System.out.println(secondBlock);
         int thirdBlock = (int) (Math.random() * 4);
-        System.out.println(thirdBlock);
         int fourthBlock = (int) (Math.random() * 4);
+        System.out.println(firstBlock);
+        System.out.println(secondBlock);
+        System.out.println(thirdBlock);
         System.out.println(fourthBlock);
+
+        MyPin pin = new MyPin(1, firstBlock, guess);
+        MyPin secondPin = new MyPin(2, secondBlock, guess);
+        MyPin thirdPin = new MyPin(3, thirdBlock, guess);
+        MyPin fourthPin = new MyPin(4, fourthBlock, guess);
 
         Scanner read = new Scanner(System.in);
         System.out.print("Enter 4 guesses separated by spaces: ");
@@ -32,6 +39,12 @@ public class MasterMind {
                 System.out.println("A Match");
             }
         }
+    }
 
+    private static class MyPin {
+
+        public MyPin(int place, int colour, int guess) {
+            System.out.println("hi");
+        }
     }
 }
