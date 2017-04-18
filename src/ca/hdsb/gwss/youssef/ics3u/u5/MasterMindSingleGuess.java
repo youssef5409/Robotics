9e7cb3d77eedfa8fb4fdd659b4192ec83504e34a
1mohamedyou <1mohamedyou@GWS03918.ihdsb.CA>
2017-04-18 09:36:16 -0400

    Finished mastermind with single guess option

--- src/ca/hdsb/gwss/youssef/ics3u/u5/MasterMindSingleGuess.java
+++ src/ca/hdsb/gwss/youssef/ics3u/u5/MasterMindSingleGuess.java
@@ -12,19 +12,24 @@ import java.util.Scanner;
  *
  * @author Youss
  */
-public class MasterMind {
+public class MasterMindSingleGuess {
 
-    public void play() {
+    public static void main(String[] args) {
         boolean allRight = false;
         int attempts = 0;
         String guess;
         int placeAndColour;
         int colour;
 
-        Scanner read = new Scanner(System.in);
         ArrayList coloursPinsHave = new ArrayList();
         ArrayList coloursArray = new ArrayList();
         ArrayList pinsArray = new ArrayList();
+        ArrayList<String> numbers = new ArrayList();
+        
+        numbers.add("first");
+        numbers.add("second");
+        numbers.add("third");
+        numbers.add("fourth");
 
         coloursArray.add("blue");
         coloursArray.add("yellow");
@@ -49,12 +54,10 @@ public class MasterMind {
             for (int i = 0; i < 4; i++) {
                 coloursPinsHave.add(pinsArray.get(i));
             }
-            System.out.println("Enter 4 guesses separated by spaces");
             System.out.println("Guesses can be \"green\", \"blue\", \"red\" "
-                    + "or \"yellow\")");
-            System.out.print("Any guesses after 4 guesses will be ignored: ");
+                    + "or \"yellow\"");
             for (int i = 0; i < 4; i++) {
-                guess = read.next().toLowerCase();
+                guess = getGuess(coloursArray, numbers.get(i));
                 if (checkGuessAndSpot(pinsArray, guess, i)) {
                     placeAndColour++;
                 }
@@ -63,7 +66,6 @@ public class MasterMind {
                     coloursPinsHave.remove(guess);
                 }
             }
-            read.nextLine();
             System.out.println("You guessed " + placeAndColour + " spots and colours correctly");
             System.out.println("You guessed " + colour + " colours correctly");
             if (placeAndColour == 4) {
@@ -71,6 +73,24 @@ public class MasterMind {
             }
         }
         System.out.println("Congratulations! It took you " + attempts + " attempt(s) to win!");
+    }
+
+    private static String getGuess(ArrayList coloursArray, String number) {
+        String guess;
+        boolean verified = false;
+        Scanner read = new Scanner(System.in);
+        do {
+            System.out.print("Enter the " + number + " guess (Anything after the guess will"
+                    + " be ignored): ");
+            guess = read.next().toLowerCase();
+            read.nextLine();
+            if (!(coloursArray.contains(guess))) {
+                System.out.println("Sorry, invalid guess");
+            } else {
+                verified = true;
+            }
+        } while (!verified);
+        return guess;
     }
 
     private static boolean checkGuess(ArrayList coloursPinsHave, String guess) {
