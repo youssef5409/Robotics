bfb4884d55c2cfd5616cb6d99ca8b5d1c46ca515
1mohamedyou <1mohamedyou@GWS03918.ihdsb.CA>
2017-04-19 15:17:09 -0400

    NO STRING TOKENIZER!

---
+++ src/ca/hdsb/gwss/youssef/ics3u/u5/MasterMind1.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.hdsb.gwss.youssef.ics3u.u5;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+/**
+ *
+ * @author Youss
+ */
+public class MasterMind1 {
+
+    public void play() {
+        boolean valid = false;
+        boolean allRight = false;
+        int attempts = 0;
+        String guess;
+        int placeAndColour;
+        int colour;
+
+        Scanner read = new Scanner(System.in);
+        ArrayList coloursPinsHave = new ArrayList();
+        ArrayList coloursArray = new ArrayList();
+        ArrayList pinsArray = new ArrayList();
+
+        coloursArray.add("blue");
+        coloursArray.add("yellow");
+        coloursArray.add("red");
+        coloursArray.add("green");
+
+        pinsArray.add(coloursArray.get(randomWholeNumber()));
+        pinsArray.add(coloursArray.get(randomWholeNumber()));
+        pinsArray.add(coloursArray.get(randomWholeNumber()));
+        pinsArray.add(coloursArray.get(randomWholeNumber()));
+
+        System.out.println(pinsArray.get(0));
+        System.out.println(pinsArray.get(1));
+        System.out.println(pinsArray.get(2));
+        System.out.println(pinsArray.get(3));
+
+        while (!allRight) {
+            placeAndColour = 0;
+            colour = 0;
+            attempts++;
+            coloursPinsHave.clear();
+            for (int i = 0; i < 4; i++) {
+                coloursPinsHave.add(pinsArray.get(i));
+            }
+            System.out.println("Enter 4 guesses separated by spaces");
+            System.out.println("Guesses can be \"green\", \"blue\", \"red\" "
+                    + "or \"yellow\")");
+            System.out.print("Any guesses after 4 guesses will be ignored: ");
+            while (!valid) {
+                ArrayList<String> guesses = new ArrayList(read.nextLine().split(" "));
+                for (int i = 0; i < 4; i++) {
+                    if (checkGuessAndSpot(pinsArray, guess, i)) {
+                        placeAndColour++;
+                    }
+                    if (checkGuess(coloursPinsHave, guess)) {
+                        colour++;
+                        coloursPinsHave.remove(guess);
+                    }
+                }
+                if () {
+                    
+                }
+            }
+            read.nextLine();
+            System.out.println("You guessed " + placeAndColour + " spots and colours correctly");
+            System.out.println("You guessed " + colour + " colours correctly");
+            if (placeAndColour == 4) {
+                allRight = true;
+            }
+        }
+        System.out.println("Congratulations! It took you " + attempts + " attempt(s) to win!");
+    }
+
+    private static boolean checkGuess(ArrayList coloursPinsHave, String guess) {
+        boolean goodGuess = false;
+        if (coloursPinsHave.contains(guess)) {
+            goodGuess = true;
+        }
+        return goodGuess;
+    }
+
+    private static boolean checkGuessAndSpot(ArrayList pinsArray, String guess, int i) {
+        boolean goodGuess = false;
+        if (pinsArray.get(i).equals(guess)) {
+            goodGuess = true;
+        }
+        return goodGuess;
+    }
+
+    private static int randomWholeNumber() {
+        int randNum = (int) (Math.random() * 4);
+        return randNum;
+    }
+}
