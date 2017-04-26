c177afbb336bbe47a03e59e20fc521a6c91cd855
1mohamedyou <1mohamedyou@GWS03918.ihdsb.CA>
2017-04-26 11:20:07 -0400

    Forgot introductory statements

---
+++ src/ca/hdsb/gwss/youssef/ics3u/u6/Unit6MainMethod.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.hdsb.gwss.youssef.ics3u.u6;
+
+/**
+ *
+ * @author 1mohamedyou
+ */
+public class Unit6MainMethod {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
