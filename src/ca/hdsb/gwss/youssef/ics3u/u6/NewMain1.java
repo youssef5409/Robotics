c54951c7c5f553ef266673f9ecda77ec26cfc3f4
1mohamedyou <1mohamedyou@GWS03918.ihdsb.CA>
2017-05-05 09:53:49 -0400

    finished frame

---
+++ src/ca/hdsb/gwss/youssef/ics3u/u6/NewMain1.java
@@ -0,0 +1,180 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.hdsb.gwss.youssef.ics3u.u6;
+
+/**
+ *
+ * @author Youss
+ */
+public class NewMain1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        maxInt();
+        maxDouble();
+
+        minInt();
+        minDouble();
+
+        sumInt();
+        sumDouble();
+
+        averageInt();
+        averageDouble();
+
+        bubbleIntAsc();
+        bubbleDoubleAsc();
+        bubbleStringAsc();
+
+        bubbleIntDsc();
+        bubbleDoubleDsc();
+        bubbleStringDsc();
+
+        selectionIntAsc();
+        selectionDoubleAsc();
+        selectionStringAsc();
+
+        selectionIntDsc();
+        selectionDoubleDsc();
+        selectionStringDsc();
+
+        linearSearchInt();
+        linearSearchDouble();
+        linearSearchString();
+
+        binarySearchInt();
+        binarySearchDouble();
+        binarySearchString();
+
+    }
+
+    private static void maxInt() {
+
+    }
+
+    private static void maxDouble() {
+
+    }
+
+    private static void minInt() {
+
+    }
+
+    private static void minDouble() {
+
+    }
+
+    private static void sumInt() {
+
+    }
+
+    private static void sumDouble() {
+
+    }
+
+    private static void averageInt() {
+
+    }
+
+    private static void averageDouble() {
+
+    }
+    
+    private static void bubbleIntAsc() {
+        
+    }
+    
+    private static void bubbleDoubleAsc() {
+
+    }
+
+    private static void bubbleStringAsc() {
+
+    }
+
+    private static void bubbleIntDsc() {
+
+    }
+
+    private static void bubbleDoubleDsc() {
+
+    }
+
+    private static void bubbleStringDsc() {
+
+    }
+
+    private static void selectionIntAsc() {
+
+    }
+
+    private static void selectionDoubleAsc() {
+
+    }
+
+    private static void selectionStringAsc() {
+
+    }
+
+    private static void selectionIntDsc() {
+
+    }
+    
+    private static void selectionDoubleDsc() {
+        
+    }
+    
+    private static void selectionStringDsc() {
+        
+    }
+    
+    private static void linearSearchInt() {
+        
+    }
+    
+    private static void linearSearchDouble() {
+        
+    }
+    
+    private static void linearSearchString() {
+        
+    }
+    
+    private static void binarySearchInt() {
+        
+    }
+    
+    private static void binarySearchDouble() {
+        
+    }
+    
+    private static void binarySearchString() { 
+        
+    }      
+    
+}
+
+
+//        //        s + char int A + int math.random * 26
+//        //Test - Bubble Sort Ascending, Integers.
+//        //PRE-CONDITIONS: Unsorted Integer Array with random values between, 
+//        //Integer.MAX_VALUE to Integer.MIN_VALUE.
+//        //POST-CONDITIONS: Sorted, from smallest to largest.
+//        System.out.println("---------------------------");
+//        System.out.println("Test #1 - Bubble Sort, int, ascending");
+//        int[] data = ArrayUtils.gRA(100, Integer.MIN_VALUE, Integer.MAX_VALUE);
+//
+//        System.out.println("BEFORE");
+//        Array.display(data);
+//        System.out.println("1");
+//        Arrays.sort(data);
+//        for (int i = 0; i < data.length; i++) {
+//            assert (1 == 2);
+//
+//        }
